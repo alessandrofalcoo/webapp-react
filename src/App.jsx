43e2bfc16b7abc73c19c4aeb5998a98b890f2551
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-16 12:05:50 +0200

    feat: add main application structure with Header, Main, and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
