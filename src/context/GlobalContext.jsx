8e205fbbe2db6b43dff0985c8263fba2d7c1e4fd
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-16 18:08:52 +0200

    feat: refactor GlobalProvider to include movie fetching functionality and improve context structure

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,7 +1,43 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useState, useEffect, use } from "react";
 
 const GlobalContext = createContext();
 
-function GlobalProvide({ children }) {
+function GlobalProvider({ children }) {
+    const [movies, setMovies] = useState([])
+    const [movie, setMovie] = useState()
 
+    const movies_api_url = `http://localhost:3000/movies/`;
+    const movie_api_url = `http://localhost:3000/movies/${movie}`
+
+
+    function fetchMovies() {
+        fetch(movies_api_url)
+            .then(res => res.json())
+            .then(data => {
+                setMovies(data)
+            })
+    }
+
+
+    function fetchMovie() {
+        fetch(movie_api_url)
+            .then(res => res.json())
+            .then(data => {
+                setMovies(data)
+            })
+    }
+
+    return (
+        <>
+            <GlobalContext.Provider value={{ movies, fetchMovies, movie, setMovie, fetchMovie }}>
+                {children}
+            </GlobalContext.Provider>
+        </>
+    )
 }
+
+function useMoviesContext() {
+    return useContext(GlobalContext)
+}
+
+export { GlobalProvider, useMoviesContext }
