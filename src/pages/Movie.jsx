dfc789199c86a29b3b90e813a1e6d57696bf1cbc
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-16 17:03:38 +0200

    fix: update image source in Home component and implement movie fetching in Movie component

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
 export default function Movie() {
+    const [movie, setMovie] = useState()
+    useEffect(() => {
+        fetch(`http://localhost/movies/${movie}`)
+            .then(res => res.json())
+            .then(data => {
+                setMovie(data)
+            })
+    }, [])
     return (
         <>
 
