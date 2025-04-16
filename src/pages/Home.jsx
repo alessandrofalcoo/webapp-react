aa23494b3cc859a80432b4f6825f581a9d216a07
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-16 17:10:10 +0200

    feat: add custom jumbotron to Main component and refactor Home and Movie components for better structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Movie from './Movie'
 
 export default function Home() {
     const [movies, setMovies] = useState([])
@@ -15,16 +16,19 @@ export default function Home() {
 
     return (
         <>
-            {movies.map(movie => (
-                <div className="card" key={movie.id}>
-                    <img className="card-img-top" src={`http://localhost:3000/img/${movie.image}`} alt="Title" />
-                    <div className="card-body">
-                        <h4 className="card-title">{movie.title}</h4>
-                        <p className="card-text">{movie.abstract}</p>
+            <div className="container d-flex gap-3 my-4 flex-column">
+                {movies.map(movie => (
+                    <div className="card" key={movie.id}>
+                        <img className="card-img-top" src={`http://localhost:3000/img/${movie.image}`} alt="Title" />
+                        <div className="card-body">
+                            <h4 className="card-title">{movie.title}</h4>
+                            <p className="card-text">{movie.abstract}</p>
+                        </div>
                     </div>
-                </div>
 
-            ))}
+                ))}
+
+            </div>
         </>
     )
 }
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -4,15 +4,22 @@ import { useEffect } from "react"
 export default function Movie() {
     const [movie, setMovie] = useState()
     useEffect(() => {
-        fetch(`http://localhost/movies/${movie}`)
-            .then(res => res.json())
-            .then(data => {
-                setMovie(data)
-            })
+        function fetchMovie() {
+            fetch(`http://localhost/movies/${movie}`)
+                .then(res => res.json())
+                .then(data => {
+                    setMovie(data)
+                })
+
+        }
     }, [])
+
+
     return (
         <>
+            <div className="container">
 
+            </div>
         </>
     )
 }
