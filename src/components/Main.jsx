d7bd3193a5a856f803f3190eb8b3a008f9da072d
alessandrofalcoo <alefalco.1003@gmail.com>
2025-04-16 12:37:48 +0200

    feat: implement Home component to fetch and display movies

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,11 @@
+import Home from "../pages/Home";
+import Footer from "./Footer";
+import Header from "./Header";
+
 export default function Main() {
     return (
         <>
-
+            <Home />
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
+    const [movies, setMovies] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:3000/movies/')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setMovies(data)
+            })
+
+    }, [])
+
+
     return (
         <>
+            {movies.map(movie => (
+                <div class="card" key={movie.id}>
+                    <img class="card-img-top" src="" alt="Title" />
+                    <div class="card-body">
+                        <h4 class="card-title">{movie.title}</h4>
+                        <p class="card-text">{movie.abstract}</p>
+                    </div>
+                </div>
 
+            ))}
         </>
     )
 }
